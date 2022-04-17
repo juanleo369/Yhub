6558f84e08a659a125401c1dbaf5f17ff6c9ca83
JUAN LEONARDO ALVARADO MUNOZ <alvarado_juan@unah.hn>
2022-04-17 17:03:25 -0600

    f

--- yahoohub/Win.yahoohub/Reportes/FormReporteOrdenes.cs
+++ yahoohub/Win.yahoohub/Reportes/FormReporteOrdenes.cs
@@ -74,57 +74,6 @@ namespace Win.yahoohub
         private void button1_Click(object sender, EventArgs e)
         {
 
-            //int id = Convert.ToInt32(textBox1.Text);
-
-            //var _orden = new OrdenesBL();
-            //var _empleado = new EmpleadosBL();
-            //var _cliente = new ClientesBL();
-            //var _estadoPedidos = new EstadoPedidosBL();
-            //var _tipoPago = new TipoPagosBL();
-            //var _impuesto = new ImpuestosBL();
-            //var _producto = new ProductosBL();
-            //var _categoria = new CategoriasBL();
-
-
-
-            //var bindingSource = new BindingSource();
-            //bindingSource.DataSource = _orden.ObtenerOrdenes(id);
-         
-            //var bindingSource1 = new BindingSource();
-            //bindingSource1.DataSource = _empleado.ObtenerEmpleados();
-
-            //var bindingSource2 = new BindingSource();
-            //bindingSource2.DataSource = _cliente.ObtenerClientes();
-
-            //var bindingSource3 = new BindingSource();
-            //bindingSource3.DataSource = _estadoPedidos.ObtenerEstadoPedidos();
-
-            //var bindingSource4 = new BindingSource();
-            //bindingSource4.DataSource = _tipoPago.ObtenerTipoPagos();
-
-            //var bindingSource5 = new BindingSource();
-            //bindingSource5.DataSource = _impuesto.ObtenerImpuestos();
-
-            //var bindingSource6 = new BindingSource();
-            //bindingSource6.DataSource = _producto.ObtenerProductos();
-
-            //var bindingSource7 = new BindingSource();
-            //bindingSource7.DataSource = _categoria.ObtenerCategorias();
-
-
-            //var reporte = new ReporteOrdenes();
-            //// reporte.SetDataSource(bindingSource);
-            //reporte.Database.Tables["Orden"].SetDataSource(bindingSource);
-            //reporte.Database.Tables["Empleado"].SetDataSource(bindingSource1);
-            //reporte.Database.Tables["Cliente"].SetDataSource(bindingSource2);
-            //reporte.Database.Tables["EstadoPedido"].SetDataSource(bindingSource3);
-            //reporte.Database.Tables["TipoPago"].SetDataSource(bindingSource4);
-            //reporte.Database.Tables["Impuesto"].SetDataSource(bindingSource5);
-            //reporte.Database.Tables["Producto"].SetDataSource(bindingSource6);
-            //reporte.Database.Tables["Categoria"].SetDataSource(bindingSource7);
-
-            //crystalReportViewer1.ReportSource = reporte;
-            //crystalReportViewer1.RefreshReport();
         }
 
         private void button7_Click(object sender, EventArgs e)
